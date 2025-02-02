0b81c28e1821634b430f09fcc3e9f1024aa6fa7c
nicolysantus <nico_ly25@hotmail.com>
2025-02-02 00:36:49 -0300

    fix(ManualModel): Ajuste nos campos String

--- back-end/Models/ManualModel.cs
+++ back-end/Models/ManualModel.cs
@@ -3,8 +3,8 @@
     public class ManualModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public string? ImageUrl { get; set; } 
         public string? ManualPdfUrl { get; set; } 
         public string? VideoUrl { get; set; } 
